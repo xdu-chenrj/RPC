30f4dbd02dbae2bc1fdf25c3d02669b2ecbd6df9
xdu-chenrj <chenrj.xdu@gmail.com>
2023-02-21 21:16:55 +0800

    improved rpcresonse class

--- src/main/java/org/dto/RPCResponse.java
+++ src/main/java/org/dto/RPCResponse.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.common.RPCResponseCodeConstant;
 
 import java.io.Serializable;
 
@@ -13,18 +14,21 @@ import java.io.Serializable;
 @Builder
 public class RPCResponse<T> implements Serializable {
 
+    private static final long serialVersionUID = 200967137377196021L;
     private String requestId;
     private Integer code;
     private String message;
     private T data;
 
     public static <T> RPCResponse<T> success(T data, String requestId) {
-        RPCResponse<T> response = new RPCResponseBuilder<T>().build();
-        if(null != data) response.setData(data);
+        RPCResponse<T> response = new RPCResponseBuilder<T>().code(RPCResponseCodeConstant.SUCCESS.getCode()).message(RPCResponseCodeConstant.SUCCESS.getMessage()).build();
+        response.setRequestId(requestId);
+        if (null != data) response.setData(data);
         return response;
     }
+
     public static <T> RPCResponse<T> fail(Integer code, String message) {
-        RPCResponse<T> response = new RPCResponseBuilder<T>().build();
+        RPCResponse<T> response = new RPCResponseBuilder<T>().code(code).message(message).build();
         return response;
     }
 }
