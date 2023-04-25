7313619c785349d4500684c4054c842cecf7266d
xdu-chenrj <chenrj.xdu@gmail.com>
2023-04-25 08:34:44 +0800

    add RandomLoadBalance

--- src/main/java/org/loadbalance/loadbalancer/RandomLoadBalance.java
+++ src/main/java/org/loadbalance/loadbalancer/RandomLoadBalance.java
@@ -4,10 +4,12 @@ import org.loadbalance.AbstractLoadBalance;
 import org.remote.dto.RpcRequest;
 
 import java.util.List;
+import java.util.Random;
 
 public class RandomLoadBalance extends AbstractLoadBalance {
     @Override
     protected String doSelect(List<String> serviceListAddress, RpcRequest rpcRequest) {
-        return null;
+        Random random = new Random();
+        return serviceListAddress.get(random.nextInt(serviceListAddress.size()));
     }
 }
