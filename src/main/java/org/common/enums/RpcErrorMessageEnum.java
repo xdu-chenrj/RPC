2bdde18e99f729df2fc558cd6f2fb6c32f8dd6fc
xdu-chenrj <chenrj.xdu@gmail.com>
2023-04-27 14:51:27 +0800

    RpcClientProxy

--- src/main/java/org/common/enums/RpcErrorMessageEnum.java
+++ src/main/java/org/common/enums/RpcErrorMessageEnum.java
@@ -7,7 +7,9 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum RpcErrorMessageEnum {
-    SERVER_CAN_NOT_BE_FOUND("sever can't not be found");
+    SERVER_CAN_NOT_BE_FOUND("sever can't not be found"),
+    SERVICE_INVOCATION_FAILURE("service invocation failure"),
+    REQUEST_NOT_MATCH_RESPONSE("request not match response");
 
     private final String message;
 }
