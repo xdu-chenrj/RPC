134d10efe2ced9c71120ce30700f5367cb537687
xdu-chenrj <chenrj.xdu@gmail.com>
2023-04-27 17:02:26 +0800

    scan custom annotations

--- src/main/java/org/annotation/RpcScan.java
+++ src/main/java/org/annotation/RpcScan.java
@@ -1,4 +1,17 @@
 package org.annotation;
 
+import org.spring.CustomScannerRegister;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+/**
+ * scan custom annotations
+ */
+@Documented
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Import(CustomScannerRegister.class)
 public @interface RpcScan {
+    String[] basePackages();
 }
