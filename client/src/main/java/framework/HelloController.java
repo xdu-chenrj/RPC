1c2ae7b7f4180687413639adf27256af19c4db69
xdu-chenrj <chenrj.xdu@gmail.com>
2023-05-10 09:13:03 +0800

    add socket client and server

--- client/src/main/java/framework/HelloController.java
+++ client/src/main/java/framework/HelloController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class HelloController {
-
     @RpcReference(version = "version", group = "test")
     private HelloService helloService;
 
